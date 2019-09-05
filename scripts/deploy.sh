dbeaaddbcc8e26e25b6eace99b7ba490994bdd09
Gimpneek <colin@gimpneek.com>
2019-09-05 22:07:33 +0100

    - Adding script to generate diff between resume versions - Adding script to generate HTML version of the diff - Adding workflow to publish HTML diff to website

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,3 +4,5 @@
 curl --ftp-create-dirs -T out/resume.html -u $FTP_USER:$FTP_PASS ftp://$FTP_HOST/$FTP_DIRECTORY/awesome/index.html
 # Upload PDF
 curl --ftp-create-dirs -T out/CV.pdf -u $FTP_USER:$FTP_PASS ftp://$FTP_HOST/$FTP_DIRECTORY/docs/CV.pdf
+# Upload LinkedIn Changes
+curl --ftp-create-dirs -T out/linkedin-changes.html -u $FTP_USER:$FTP_PASS ftp://$FTP_HOST/$FTP_DIRECTORY/docs/linkedin-changes.html
